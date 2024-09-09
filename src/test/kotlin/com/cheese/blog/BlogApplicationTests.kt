d95e4f82ced63264d2ae0c2c25118c0328dd2018
xheeese <xeese.tech@gmail.com>
2024-09-09 16:44:46 +0900

    Initial commit

---
+++ src/test/kotlin/com/cheese/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cheese.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
